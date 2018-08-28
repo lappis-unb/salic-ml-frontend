46119367e2188bcb4c6bef4a4d6a5ce10c088b7f
Eduardo Nunes <eduardonunes2525@gmail.com>
2018-08-27 21:53:38 -0300

    Add static files and modify Show Metrics layout

--- src/views/ShowMetrics.vue
+++ src/views/ShowMetrics.vue
@@ -2,18 +2,30 @@
   <div class="show-metrics">
     <header-show />
     <complexity-title />
+    <metrics-list />
+    <project-feedback />
+    <user-login />
+    <success-message/>
   </div>
 </template>
 
 <script>
 import HeaderShow from '@/components/show_metrics/HeaderShow.vue'
 import ComplexityTitle from '@/components/show_metrics/ComplexityTitle.vue'
+import MetricsList from '@/components/show_metrics/MetricsList.vue';
+import ProjectFeedback from '@/components/show_metrics/ProjectFeedback.vue';
+import UserLogin from '@/components/show_metrics/UserLogin.vue';
+import SuccessMessage from '@/components/show_metrics/SuccessMessage.vue';
 
 export default {
   name: 'show-metrics',
   components: {
     'header-show': HeaderShow,
-    'complexity-title': ComplexityTitle
+    'complexity-title': ComplexityTitle,
+    'metrics-list': MetricsList,
+    'project-feedback': ProjectFeedback,
+    'user-login': UserLogin,
+    'success-message': SuccessMessage
   }
 }
 
