d3a25d9d4ca56dfbf0851d6619181f3a512ffcb0
Eduardo Nunes <eduardonunes2525@gmail.com>
2018-08-27 21:52:45 -0300

    Add metric list component

---
+++ src/components/show_metrics/MetricsList.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="ui basic segment" id="DiagnosticArea">
+        <div class="ui styled fluid accordion" id="DiagnosticMetrics">
+            <metric-card />
+        </div>
+    </div>
+</template>
+
+<script>
+import MetricCard from "@/components/show_metrics/MetricCard.vue"
+
+export default {
+  name: "MetricsList",
+  data: function() {
+    return {};
+  },
+  components: {
+      "metric-card": MetricCard
+  }
+};
+</script>
