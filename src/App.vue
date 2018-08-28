54861475ba1896b891ef13a43e35284ed828e25b
Eduardo Nunes <eduardonunes2525@gmail.com>
2018-08-27 21:53:38 -0300

    Add static files and modify Show Metrics layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div class="container-application">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/show">show</router-link>
