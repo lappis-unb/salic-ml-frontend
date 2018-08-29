b25cabec205f82d7db84c7611b3a37f736a49251
Eduardo Nunes <eduardonunes2525@gmail.com>
2018-08-28 21:43:52 -0300

    Add new layout in project feedback component

--- src/components/show_metrics/feedback/ProjectFeedback.vue
+++ src/components/show_metrics/feedback/ProjectFeedback.vue
@@ -3,17 +3,15 @@
         <div class="ui equal width form project-feedback-container">
             <div class="header">
                 Na sua opinião, como você avalia
-                <!-- <em>&ldquo;{{ project.name }}&rdquo;</em>: -->
+                <em>&ldquo;{{ project.name }}&rdquo;</em>:
             </div>
             <div class="inline fields">
-                <!-- {% for grade in project_feedback_list %}
-                <div class="field">
+                <div class="field" v-for="(grade, index) in project_feedback_list" :key="grade+index">
                     <div class="ui radio checkbox">
-                        <input type="radio" required name="project_feedback_grade" value={{forloop.counter}}>
+                        <input type="radio" required name="project_feedback_grade">
                         <label>{{ grade }}</label>
                     </div>
                 </div>
-                {% endfor %} -->
             </div>
         </div>
         <!-- onmouseover="getRating()" href="#first-star"  -->
@@ -24,8 +22,32 @@
 </template>
 
 <script>
-
 export default {
   name: "ProjectFeedback",
+  data: function() {
+    return {
+      project: {
+        name: "Qualquer"
+      },
+      project_feedback_list:  ['Muito simples', 'Simples', 'Normal', 'Complexo', 'Muito complexo']
+    };
+  }
+  //   props: {
+  //       project: Object
+  //   }
 };
 </script>
+<style>
+#FinalForm{
+	margin: 3em !important;
+    text-align: center !important;
+}
+
+#FinalForm .header{
+    font-size: 1.2em !important;
+}
+
+.project-feedback-container{
+	margin-bottom: 4em !important;
+}
+</style>
