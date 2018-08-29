076404fe95e58b85907e07547d4af495eea99d66
Eduardo Nunes <eduardonunes2525@gmail.com>
2018-08-29 11:40:48 -0300

    Add radial indicator in complexity title component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div class="container-application">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/show">show</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
--- src/views/ShowMetrics.vue
+++ src/views/ShowMetrics.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="show-metrics">
     <header-show />
-    <complexity-title />
+    <complexity-title :value="indicator.value" />
     <metrics-list />
     <project-feedback />
-    <!-- <user-login />
-    <success-message/> -->
   </div>
 </template>
 
@@ -15,9 +13,6 @@ import ComplexityTitle from '@/components/show_metrics/ComplexityTitle.vue'
 import MetricsList from '@/components/show_metrics/MetricsList.vue';
 import ProjectFeedback from '@/components/show_metrics/feedback/ProjectFeedback.vue';
 
-// import UserLogin from '@/components/show_metrics/UserLogin.vue';
-// import SuccessMessage from '@/components/show_metrics/SuccessMessage.vue';
-
 export default {
   name: 'show-metrics',
   components: {
@@ -25,8 +20,13 @@ export default {
     'complexity-title': ComplexityTitle,
     'metrics-list': MetricsList,
     'project-feedback': ProjectFeedback,
-    // 'user-login': UserLogin,
-    // 'success-message': SuccessMessage
+  },
+  data: function(){
+    return {
+      indicator: {
+        value: 77
+      }
+    }
   }
 }
 
