4ba251b4606a59be0f9c9760ba2f6b38baea2e3d
Eduardo Nunes <eduardonunes2525@gmail.com>
2018-08-28 21:07:44 -0300

    Improve layout and change location of feedback components

---
+++ src/components/show_metrics/feedback/FeedbackForm.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="ui fedback form">
+        <div class="header">Quanto essa informação é útil para você?</div>
+        <div class="fields">
+            <div class="four wide field rating-star">
+                <div class="ui massive star rating" name="itens_orcamentarios_rating"></div>
+            </div>
+            <div class="inline field">
+                <input placeholder="Por quê?" type="text" size="50" name="itens_orcamentarios_text">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "FeedbackForm",
+    mounted: function(){
+        $('.ui.rating').rating({
+            initialRating: 0,
+            maxRating: 5
+        });
+    }
+}
+</script>
+
+<style>
+/*************************** 
+ ********** Forms **********
+ ***************************/
+
+.form .divider{
+	margin-top: 2em !important;
+}
+
+.ui.form .header{
+	display: block !important;
+	margin-bottom: 1em !important;
+}
+
+.star.rating{
+	padding-top: 6px;
+	width: 90%;
+}
+</style>
+
+
---
+++ src/components/show_metrics/feedback/SuccessMessage.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="ui info message">
+    <i class="close icon"></i>
+    <div class="header">
+        Sucesso ao enviar o feedback
+    </div>
+    <ul class="list">
+        <li>Agradecemos pelo seu feedback!</li>
+    </ul>
+</div>
+</template>
+
+<script>
+export default {
+  name: "SuccessMessage",
+  mounted: function() {
+    $(".message .close").on("click", function() {
+      $(this)
+        .closest(".message")
+        .transition("fade");
+    });
+  }
+};
+</script>
