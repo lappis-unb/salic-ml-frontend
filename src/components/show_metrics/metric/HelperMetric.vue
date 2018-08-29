2ac0bf8ee3e9dd8dd57912bea1ced9a9e7c5d689
Eduardo Nunes <eduardonunes2525@gmail.com>
2018-08-28 21:08:38 -0300

    Fix layout of metric list and change project structure

---
+++ src/components/show_metrics/metric/HelperMetric.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="helper">
+        <i class="question circle outline icon"></i>
+        <div class="ui floating message">
+            <p>{{helper_text}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HelperMetric",
+    props: {
+        helper_text: String
+    }
+}
+</script>
+
+<style>
+
+/*************************** 
+ ****** Help Message *******
+ ***************************/
+.helper{
+	display: inline !important;
+}
+
+.helper .question.icon{
+	color: #999;
+	font-size: 1em;
+	margin-left: 0.5em;
+}
+
+.helper:hover .question.icon{
+	color: #2185d0;
+}
+ 
+.helper .ui.message{
+	background-color: #dff0ff;
+	border-color: #2185d0;
+	color: #2185d0;
+	font-size: 0.6em;
+	font-weight: normal;
+	left: 25%;
+	margin: 0;
+	opacity: 0;
+	position: absolute;
+	right: 0;
+	transition: opacity 1s;
+	visibility: hidden;
+	width: 50%;
+	z-index: 999;
+}
+
+.helper:hover .ui.message{
+	opacity: 1;
+	visibility: visible;
+}
+
+</style>
+
+
---
+++ src/components/show_metrics/metric/MetricCardContent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+		<p>
+			Esta quantidade de item orçamentários está dentro
+			da margem considerada como normal sendo esta: de.
+		</p>
+	</div>
+</template>
+
+<script>
+export default {
+  name: "MetricCardContent",
+//   props: {
+//     content: String
+//   }
+};
+</script>
+
+<style>
+</style>
+
+
