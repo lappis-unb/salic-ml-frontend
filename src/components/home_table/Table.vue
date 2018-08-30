9c1b746deb538a29eb6971e1dd3d092aa3fe4e76
Eduardo Nunes <eduardonunes2525@gmail.com>
2018-08-30 00:08:03 -0300

    Improve vuetable with api fields

--- src/components/home_table/Table.vue
+++ src/components/home_table/Table.vue
@@ -1,7 +1,7 @@
 
 <template>
   <div class="ui container">
-
+<!-- api-url="http://localhost:3000/projects" -->
     <filter-bar></filter-bar>
     <vuetable ref="vuetable"
       api-url="https://vuetable.ratiw.net/api/users"
@@ -11,26 +11,9 @@
       :multi-sort="true"
       :sort-order="sortOrder"
       :append-params="moreParams"
-      detail-row-component="my-detail-row"
       @vuetable:cell-clicked="onCellClicked"
       @vuetable:pagination-data="onPaginationData"
     >
-      <template slot="actions" scope="props">
-        <div class="custom-actions">
-          <button class="ui basic button"
-            @click="onAction('view-item', props.rowData, props.rowIndex)">
-            <i class="zoom icon"></i>
-          </button>
-          <button class="ui basic button"
-            @click="onAction('edit-item', props.rowData, props.rowIndex)">
-            <i class="edit icon"></i>
-          </button>
-          <button class="ui basic button"
-            @click="onAction('delete-item', props.rowData, props.rowIndex)">
-            <i class="delete icon"></i>
-          </button>
-        </div>
-      </template>
     </vuetable>
     <div class="vuetable-pagination ui basic segment grid">
       <vuetable-pagination-info ref="paginationInfo"
@@ -50,13 +33,9 @@ import VueEvents from 'vue-events'
 import Vuetable from 'vuetable-2/src/components/Vuetable'
 import VuetablePagination from 'vuetable-2/src/components/VuetablePagination'
 import VuetablePaginationInfo from 'vuetable-2/src/components/VuetablePaginationInfo'
-import CustomActions from './CustomActions'
-import DetailRow from './DetailRow'
 import FilterBar from './FilterBar'
 
 Vue.use(VueEvents)
-Vue.component('custom-actions', CustomActions)
-Vue.component('my-detail-row', DetailRow)
 Vue.component('filter-bar', FilterBar)
 
 export default {
@@ -69,66 +48,33 @@ export default {
     return {
       fields: [
         {
-          name: '__handle',
+          name: 'pronac',
+          sortField: 'pronac',
           titleClass: 'center aligned',
-          dataClass: 'center aligned'
+          dataClass: 'center aligned',
+          callback: 'pronacLabel'
         },
         {
-          name: '__sequence',
-          title: '#',
+          name: 'complexity',
+          sortField: 'complexity',
           titleClass: 'center aligned',
-          dataClass: 'right aligned'
-        },
-        {
-          name: '__checkbox',
-          titleClass: 'center aligned',
-          dataClass: 'center aligned'
-        },
-        {
-          name: 'name',
-          sortField: 'name',
+          dataClass: 'center aligned',
         }, 
         {
-          name: 'email',
-          sortField: 'email'
+          name: 'name',
+          sortField: 'name'
         },
         {
-          name: 'birthdate',
-          sortField: 'birthdate',
-          titleClass: 'center aligned',
-          dataClass: 'center aligned',
-          callback: 'formatDate|DD-MM-YYYY'
-        },
-        {
-          name: 'nickname',
-          sortField: 'nickname',
-          callback: 'allcap'
-        },
-        {
-          name: 'gender',
-          sortField: 'gender',
-          titleClass: 'center aligned',
-          dataClass: 'center aligned',
-          callback: 'genderLabel'
-        },
-        {
-          name: 'salary',
-          sortField: 'salary',
-          titleClass: 'center aligned',
-          dataClass: 'right aligned',
-          callback: 'formatNumber'
-        },
-        {
-          name: '__slot:actions',
-          title: 'Slot Actions',
+          name: 'assignee',
+          sortField: 'assignee',
           titleClass: 'center aligned',
           dataClass: 'center aligned',
         }
       ],
       sortOrder: [
         {
-          field: 'email',
-          sortField: 'email',
+          field: 'name',
+          sortField: 'name',
           direction: 'asc'
         }
       ],
@@ -140,21 +86,8 @@ export default {
     this.$events.$on('filter-reset', e => this.onFilterReset())
   },
   methods: {
-    allcap (value) {
-      return value.toUpperCase()
-    },
-    genderLabel (value) {
-      return value === 'M'
-        ? '<span class="ui teal label"><i class="large man icon"></i>Male</span>'
-        : '<span class="ui pink label"><i class="large woman icon"></i>Female</span>'
-    },
-    formatNumber (value) {
-      return accounting.formatNumber(value, 2)
-    },
-    formatDate (value, fmt = 'D MMM YYYY') {
-      return (value == null)
-        ? ''
-        : moment(value, 'YYYY-MM-DD').format(fmt)
+    pronacLabel(value) {
+      return '<span class="ui teal label">'+ 1234 +'</span>'
     },
     onPaginationData (paginationData) {
       this.$refs.pagination.setPaginationData(paginationData)
@@ -168,10 +101,11 @@ export default {
     },
     onCellClicked (data, field, event) {
       console.log('cellClicked: ', field.name)
-      this.$refs.vuetable.toggleDetailRow(data.id)
+      //this.$refs.vuetable.toggleDetailRow(data.id)
     },
     onFilterSet (filterText) {
       this.moreParams.filter = filterText
+      console.log('Entrou aqui')
       Vue.nextTick( () => this.$refs.vuetable.refresh() )
     },
     onFilterReset () {
