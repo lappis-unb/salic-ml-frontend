e1b798c7af1987037ea01ed51c6c6462e7e7fff8
Eduardo Nunes <eduardonunes2525@gmail.com>
2018-08-29 22:14:38 -0300

    Make components dynamics

--- src/components/show_metrics/MetricsList.vue
+++ src/components/show_metrics/MetricsList.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="ui basic segment" id="DiagnosticArea">
-      <div class="ui styled fluid accordion" id="DiagnosticMetrics">
-          <div class="title active" :class="metric.check_outlier">
+      <div v-for="(metric,index) in metrics" :key="metric.name+index"
+          class="ui styled fluid accordion" id="DiagnosticMetrics">
+          <div class="title active" :class="metric.outlier_check">
               <div class="ui ribbon label">
                   <i class="icon"></i>
               </div>
-              <span>{{metric.name}}: {{metric.value}}</span>
+              <span>{{metric.name_title}}: {{metric.value}}</span>
               <helper-metric :helper_text="metric.helper_text"/>
               <i class="dropdown icon"></i>
           </div>
-          <div class="content active">
-              <metric-content />
+          <div class="content active" :class="metric.outlier_check">
+              <metric-content :metric="metric" />
               <div class="ui divider"></div>
               <feedback-form />
           </div>
@@ -19,22 +20,17 @@
 </template>
 
 <script>
-import FeedbackForm from './feedback/FeedbackForm.vue';
-import HelperMetric from './metric/HelperMetric.vue';
-import MetricCardContent from './metric/MetricCardContent.vue';
+import FeedbackForm from "./feedback/FeedbackForm.vue";
+import HelperMetric from "./metric/HelperMetric.vue";
+import MetricCardContent from "./metric/MetricCardContent.vue";
 
 export default {
   name: "MetricsList",
   data: function() {
-    return {
-        metric: {
-            name: "Itens orçamentários",
-            value: 0,
-            helper_text: "Compara a quantidade de itens deste projeto com a quantidade mais comum \
-            de itens em projetos do mesmo segmento",
-            check_outlier: "Metric-good",
-        }
-    };
+    return {};
+  },
+  props: {
+    metrics: Array
   },
   components: {
     "feedback-form": FeedbackForm,
@@ -48,7 +44,6 @@ export default {
 </script>
 
 <style>
-
 /*************************** 
  ** Diagnostic Accordion ***
  ***************************/
@@ -72,19 +67,19 @@ export default {
   box-shadow: none !important;
   margin-top: -1em !important;
   border: solid 2px yellow !;
-} 
-
-.ui.styled.accordion .accordion .title{
-	border: none !important;
-	color: red !important;
-	padding-left: 0 !important;
-    padding-bottom: 0 !important;
-    border: solid 2px yellow;
 }
 
-.ui.styled.accordion .accordion .content{
-    padding-left: 1.6em !important;
-    border: solid 2px yellow;
+.ui.styled.accordion .accordion .title {
+  border: none !important;
+  color: red !important;
+  padding-left: 0 !important;
+  padding-bottom: 0 !important;
+  border: solid 2px yellow;
+}
+
+.ui.styled.accordion .accordion .content {
+  padding-left: 1.6em !important;
+  border: solid 2px yellow;
 }
 
 /*************************** 
@@ -92,51 +87,49 @@ export default {
  ***************************/
 
 .ui.ribbon.label {
-    color: #FFF;
-	margin-left: 0;
-	left: calc(-2rem - 1.2em) !important;
-	padding: 1em 0 1em 2em !important;
+  color: #fff;
+  margin-left: 0;
+  left: calc(-2rem - 1.2em) !important;
+  padding: 1em 0 1em 2em !important;
 }
 .ui.ribbon.label .icon {
-	font-size: 3em;
-	padding-left: 0.2em;
+  font-size: 3em;
+  padding-left: 0.2em;
 }
 
 /***** Types styles *****/
 
 .title.Metric-bad {
-	color: #db2828 !important;
+  color: #db2828 !important;
 }
-.Metric-bad .ui.ribbon.label{
-	background-color: #db2828;
-	border-color: #b21e1e;
+.Metric-bad .ui.ribbon.label {
+  background-color: #db2828;
+  border-color: #b21e1e;
 }
-.Metric-bad .icon:before{
-	content: "\f2c7"; /* Thermometer full */
+.Metric-bad .icon:before {
+  content: "\f2c7"; /* Thermometer full */
 }
-
 
 .Metric-average {
-	color: #f2b01c !important;
+  color: #f2b01c !important;
 }
-.Metric-average .ui.ribbon.label{
-	background-color: #f2b01c;
-	border-color: #cf910c;
+.Metric-average .ui.ribbon.label {
+  background-color: #f2b01c;
+  border-color: #cf910c;
 }
-.Metric-average .icon:before{
-	content: "\f2c9"; /* Thermometer half full */
-} 
-
+.Metric-average .icon:before {
+  content: "\f2c9"; /* Thermometer half full */
+}
 
 .Metric-good {
-	color: #666 !important;
+  color: #666 !important;
 }
-.Metric-good .ui.ribbon.label{
-	color: #666;
-	background-color: #E7E7E7;
-	border-color: #E7E7E7;
+.Metric-good .ui.ribbon.label {
+  color: #666;
+  background-color: #e7e7e7;
+  border-color: #e7e7e7;
 }
-.Metric-good .icon:before{
-	content: "\f2cb"; /* Thermometer empty */
+.Metric-good .icon:before {
+  content: "\f2cb"; /* Thermometer empty */
 }
 </style>
