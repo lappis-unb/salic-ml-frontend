9a84f186c3b702e5fdd222efad725dde8db0dfd6
eduardonunes <eduardonunes2525@gmail.com>
2018-09-04 23:56:46 -0300

    Add submit data after stop write commit

--- src/components/show_metrics/feedback/UserLogin.vue
+++ src/components/show_metrics/feedback/UserLogin.vue
@@ -41,7 +41,7 @@ export default {
         name: "",
         email: ""
       },
-      url: "http://192.168.10.177:8080/indicators/create_single_user",
+      url: "http://localhost:8081/indicators/create_single_user",
       //url: "http://localhost:3000/user",
       message: {
         show_message: false,
