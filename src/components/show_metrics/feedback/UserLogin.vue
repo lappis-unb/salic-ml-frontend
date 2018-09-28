b4dacd5dad5200eea58edbaf27831a6402671c4b
eduardonunes <eduardonunes2525@gmail.com>
2018-09-28 00:46:09 -0300

    Fix bug of duplicate post

--- src/components/show_metrics/feedback/UserLogin.vue
+++ src/components/show_metrics/feedback/UserLogin.vue
@@ -1,38 +1,39 @@
 <template>
-<div class="ui ten column centered grid" style="margin-top:50px;">
+  <div class="ui ten column centered grid" style="margin-top:50px;">
     <feedback-message v-if="message.show_message" :message="message" />
-    <div class="ui raised segment" id="LoginArea">
+      <div class="ui raised segment" id="LoginArea">
         <div class="ui form">
-            <div class="header">
-                Preencha seus dados para avaliar este projeto (ou para continuar uma avaliação interrompida):
-            </div>
-            <form @submit.prevent="validateUser">
-                <div class="three fields">
-                    <div class="six wide field">
-                        <input :required="true" placeholder="Nome Completo" type="text" name="user_first_name" v-model="user.name">
-                    </div>
-                    <div class="seven wide field">
-                        <div class="ui right labeled input">
-                            <input :required="true" placeholder="Email" type="text" name="user_email"  v-model="user.email">
-                            <div class="ui label">
-                                @cultura.gov.br
-                            </div>
-                        </div>
-                    </div>
-                    <input type="hidden" name="project_pronac" >
-                    <div class="two wide field">
-                        <input class="ui submit button primary" type="submit" value="Enviar"  v-on:click="validateUser()">
-                    </div>
+          <div class="header">
+            Preencha seus dados para avaliar este projeto (ou para continuar uma avaliação interrompida):
+          </div>
+          <form @submit.prevent="validateUser">
+            <div class="three fields">
+              <div class="six wide field">
+                <input :required="true" placeholder="Nome Completo" type="text" name="user_first_name" v-model="user.name">
+              </div>
+              <div class="seven wide field">
+                <div class="ui right labeled input">
+                  <input :required="true" placeholder="Email" type="text" name="user_email"  v-model="user.email">
+                  <div class="ui label">
+                    @cultura.gov.br
+                  </div>
                 </div>
-            </form>
+              </div>
+              <input type="hidden" name="project_pronac" >
+              <div class="two wide field">
+                <button class="ui submit button primary" type="submit">Enviar</button>
+              </div>
+            </div>
+          </form>
         </div>
-    </div>
-</div>
+      </div>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 import FeedbackMessage from './FeedbackMessage.vue';
+
 export default {
   name: "UserLogin",
   data: function() {
@@ -45,14 +46,14 @@ export default {
       message: {
         show_message: false,
         title: "Erro",
-        type: "error", 
+        type: "error",
         text:
-          "Não foi possível conectar com a API, por favor verificar a conexão com a internet."
+        "Não foi possível conectar com a API, por favor verificar a conexão com a internet."
       }
     };
   },
   components: {
-      "feedback-message": FeedbackMessage
+    "feedback-message": FeedbackMessage
   },
   methods: {
     validateUser: function() {
@@ -60,7 +61,6 @@ export default {
     },
     updateUser: function() {
       this.$emit("setUser", this.user);
-      console.log(this.user);
       var self = this;
 
       axios
@@ -77,6 +77,6 @@ export default {
           self.message.show_message = true;
         });
     }
-  }
+   }
 };
 </script>
