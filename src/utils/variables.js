94222a45f54f43bd03d52a025a7d91e5a4fb61f9
Eduardo_Nunes <eduardonunes2525@gmail.com>
2019-03-25 09:30:09 -0300

    Fix bug in metric list

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -1,7 +1,7 @@
 // export const API_PATH_PROJECT_LIST = 'https://salicml.lappis.rocks/projetos'
 //export const API_PATH_PROJECT = 'https://salicml.lappis.rocks/projetos'
 var base_path = 'http://localhost:8000/v1/'
-//var base_path = 'http://192.168.100.48:8000/v1/'
+//var base_path = 'http://192.168.100.210:8000/v1/'
 
 export const API_PATH_PROJECT_LIST = base_path + 'projects/'
 export const API_PATH_PROJECT =  base_path + 'projects/'
