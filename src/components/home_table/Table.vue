2fe58f7f6ad5322bc173b0fa24ff0f7950011d67
eduardonunes <eduardonunes2525@gmail.com>
2019-04-08 00:39:45 -0300

    Add exceptions cases in metrics list

--- src/components/home_table/Table.vue
+++ src/components/home_table/Table.vue
@@ -116,9 +116,9 @@ export default {
     },
     complexityLabel(value) {
       this.complexity = value
-      var color = "#DB2828";
-      if(value>=80) color = "#DB2828";
-      else (value>=30) ? color = "#F2B01C" : color = "#1B5E20";
+      var color = "#1B5E20";
+      if(value>=8) color = "#DB2828";
+      else (value>=3) ? color = "#F2B01C" : color = "#1B5E20";
 
       return '<strong style="color: ' + color + '; font-size: 20px;">' + (value) + '</strong>'
     },
--- src/components/show_metrics/metrics/content/SimpleListContent.vue
+++ src/components/show_metrics/metrics/content/SimpleListContent.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div v-if="metrica.value!=0" class="ui bulleted list">
+        <div v-if="metrica.valor!=0" class="ui bulleted list">
           <ul  v-for="(item, index) in metrica.lista_de_comprovantes" :key="item+index" class="item">
             <li><a target="_blank" :href="item.link">{{item.item}} - {{((item.approved_value/item.verified_value) * 100).toFixed(2)}}%</a></li>
           </ul>
